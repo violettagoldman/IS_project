0a2089a50bc65533c593613950215ce8fcb650e6
Violetta Goldman <violagoldman@mail.ru>
2022-06-04 18:42:30 +0200

    seed_db updated with products

--- seed_db.php
+++ seed_db.php
@@ -1,29 +1,72 @@
 <?php
 include_once("db.php");
 
-// Create database
+/**
+ * Create the main database, 'mcdo'.
+ */
 $sql = "CREATE DATABASE mcdo";
-if ($conn->query($sql) === TRUE) {
+if ($conn->query($sql) === TRUE)
+{
   echo "Database created successfully";
-} else {
+}
+else
+{
   echo "Error creating database: " . $conn->error;
 }
-
 $conn->select_db("mcdo");
 
-// sql to create table
+/**
+ * Create the 'Orders' database.
+ */
 $sql = "CREATE TABLE Orders (
     id VARCHAR(3) PRIMARY KEY,
     order_status VARCHAR(30) NOT NULL,
     cart VARCHAR(1024),
     update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-    )";
-    
-if ($conn->query($sql) === TRUE) {
+    )"; 
+if ($conn->query($sql) === TRUE)
+{
     echo "Table created successfully";
-} else {
-    die("Error creating table: " . $conn->error);
-}  
+}
+else
+{
+    echo "Error creating table: " . $conn->error;
+}
+
+/**
+ * Create the 'Products' table.
+ */
+$sql = "CREATE TABLE Products (
+  id INT(5) UNSIGNED PRIMARY KEY,
+  category VARCHAR(50),
+  item VARCHAR(50),
+  item_description VARCHAR(200),
+  price FLOAT(4) NOT NULL,
+  image_url VARCHAR(1000),
+  update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  )"; 
+if ($conn->query($sql) === TRUE)
+{
+  echo "Table created successfully";
+}
+else
+{
+  echo "Error creating table: " . $conn->error;
+}
+
+/**
+ * Import the products into the 'Products' table.
+ */
+$csv = fopen("./products.csv", "r");
+while (($row = fgetcsv($csv)) !== FALSE)
+{
+  $row[4] = floatval(str_replace(",", ".", $row[4]));
+  $sql = "INSERT INTO `Products` (`id`, `category`, `item`, `item_description`, `price`, `image_url`) values ($row[0], \"$row[1]\", \"$row[2]\", \"$row[3]\", $row[4], \"$row[5]\")";
+  if ($conn->query($sql) !== TRUE)
+  {
+    echo "Cannot insert line. SQL: $sql<br />Error: $conn->error<br />";
+  }
+}
 
 $conn->close();
 
