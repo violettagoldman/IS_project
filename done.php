2f310c2f72c2c17c4b436b0edef0002236984601
Violetta Goldman <violagoldman@mail.ru>
2022-06-14 21:08:00 +0200

    merge css

---
+++ done.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+ini_set("display_errors", 1);
+
+require_once("db.php");
+include_once("Order.php");
+include_once("Product.php");
+$order = Order::get_instance();
+$products = Product::get_all_products();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="author" content="Shana">
+  <title>Mcdonald's Borne</title>
+  <link rel="stylesheet" href="CSS/style.css">
+</head>
+
+<body>
+ 
+ 
+ <div id="background">
+ </div>
+
+<div style="margin-left:25%;padding:1px 16px;height:1000px;">
+  
+  <div class="header">
+  <h1>✅ Validé</h1>
+  </div>
+  
+  <h3>Vous avez validé votre panier :</h3>
+
+  <p>Merci pour votre commande ! Nous vous préparons vos plats au plus vite pour une meilleure dégustation 🍔😋🍟</p>
+
+  <p class="order"> Allez à la caisse et payez : 
+    <span> <?php
+       echo $order->get_cart()->get_total();
+    ?>€</span>
+  </p>
+
+  <p class="order"> Votre numéro de commande est le : 
+    <span> <?php
+      echo $order->get_id();
+    ?></span>
+  </p>
+  <p> 👋 A très bientôt !</p>
+
+  <a href="index.php?reset">Passer une nouvelle commande</a>
+
+</div>
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -22,6 +22,13 @@ if (ISSET($_GET["cart_update"]))
     }
     $order->save();
 }
+
+if (ISSET($_GET["reset"]))
+{
+    session_destroy();
+    header('Location: /');
+    exit();
+}
 ?>
 
 <!DOCTYPE html>
@@ -31,22 +38,47 @@ if (ISSET($_GET["cart_update"]))
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="CSS/style.css">
 </head>
+
 <body>
+<ul>
+ <li><a class='active' href='/'>🍟 Produits</a></li>
+  <li><a href='done.php'>✅ Valider</a></li>
+ </ul>
+ 
+ <div id='background'>
+ </div>
 
-    <?php
-    echo "<p>Total Price is: {$order->get_cart()->get_total()}</p>";
-    foreach ($products as $product)
-        echo "<div class=\"item\">
-            <img src=\"{$product->get_image_url()}\" />
-            <span>{$product->get_item()}</span>
-            <span>{$product->get_item_description()}</span>
-            <span>{$product->get_price()}€</span>
-            <a href=\"?cart_update={$product->get_id()}+\">+</a>
-            <span>{$order->get_cart()->get_item($product->get_id())->get_quantity()}</span>
-            <a href=\"?cart_update={$product->get_id()}-\">-</a>
-        </div>";
-    ?>
+<div style='margin-left:25%;padding:1px 16px;height:1000px;'>
+  
+  <div class='header'>
+  <h1>🍟 Nos Produits</h1>
+  </div>
+  
+  <h3>Veuillez sélectionner nos produits</h3>
 
+  <p>Une fois satisfait.e de votre panier, vous pouvez <b>valider</b> votre commande 🙂.</p>
+
+<?php
+  echo "<p>Total Price is: {$order->get_cart()->get_total()}€</p>"
+?>
+
+  <div class='container'>
+  <?php
+  foreach ($products as $product) {
+  echo "
+  <div class='card'>
+   <img src=\"{$product->get_image_url()}\" style='width:180px; height:180px; object-fit: scale-down;'>
+   <h4><b>{$product->get_item()}</b></h4> 
+   <span>{$product->get_item_description()}</span>
+   <p class='price'><b>{$product->get_price()}€</b></p> 
+   <a class='buttonPlus' href='?cart_update={$product->get_id()}+'>+</a>
+   <span>{$order->get_cart()->get_item($product->get_id())->get_quantity()}</span>
+   <a class='buttonMinus' href='?cart_update={$product->get_id()}-'>-</a>
+  </div>";
+  }
+?>
+</div>
 </body>
 </html>
